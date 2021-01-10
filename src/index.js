74b7f9bc40486aef8c2bb37eeff9f748c9eb032e
Krona Emmanuel <kronaemmanuel@gmail.com>
2021-01-10 18:38:43 +0500

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,111 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import todoData from "./data";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class TodoItem extends React.Component {
+  render() {
+    const completedStyle = {
+      textDecoration: "line-through",
+    };
+    return (
+      <p className="todoitem">
+        <input
+          type="checkbox"
+          checked={this.props.item.completed}
+          onChange={() => this.props.handleChange(this.props.item.id)}
+          id={this.props.item.id}
+        ></input>
+        <label
+          style={this.props.item.completed ? completedStyle : null}
+          htmlFor={this.props.item.id}
+        >
+          {this.props.item.title}
+        </label>
+      </p>
+    );
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: todoData,
+      newItem: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/todos?userId=1")
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({
+          data: json,
+          newItem: "",
+        });
+      });
+  }
+
+  handleChange = (itemId) => {
+    this.setState((prevState) => {
+      const newState = prevState.data.map((item) => {
+        if (item.id === itemId) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      });
+
+      return { data: newState };
+    });
+  };
+
+  handleInput = (e) => {
+    this.setState({
+      newItem: e.target.value,
+    });
+  };
+
+  addNewTodoItem = (e) => {
+    e.preventDefault();
+    this.setState((prevState) => {
+      return {
+        data: [
+          ...prevState.data,
+          {
+            id: prevState.data.length + 1,
+            title: this.state.newItem,
+            completed: false,
+          },
+        ],
+        newItem: "",
+      };
+    });
+  };
+
+  render() {
+    const todoList = this.state.data.map((item) => (
+      <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
+    ));
+
+    return (
+      <form className="todolist-container">
+        <div className="todolist-add">
+          <input
+            name="newItem"
+            placeholder="Enter new Item"
+            value={this.state.newItem}
+            onChange={this.handleInput}
+          />
+          <button onClick={this.addNewTodoItem}>Add</button>
+        </div>
+        {todoList}
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
